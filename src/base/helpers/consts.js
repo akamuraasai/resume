4306aef5cbeca508a48891e14b201e6b0f3ecc5d
Jonathan Willian <jonathan.willian.tod@gmail.com>
2017-10-05 08:32:12 -0300

    started to make a initial state

---
+++ src/base/helpers/consts.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/prefer-default-export
+export const INITIAL_RESUME = {
+  name: '',
+  sections: [
+    {
+      type: 'about',
+      description: '',
+      items: [
+        {
+          type: 'sign',
+          value: 'Jonathan Willian',
+        },
+        {
+          type: 'infos',
+          values: [
+            {
+              title: 'Nickname',
+              value: 'AkamuraAsai',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
--- src/base/sections/Skills.jsx
+++ src/base/sections/Skills.jsx
@@ -1,39 +1,28 @@
+/* eslint-disable camelcase,react/prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
 import { Container } from 'semantic-ui-react';
 import Section from '../layouts/Section';
 import SectionInfo from '../components/SectionInfo';
 import SkillList from '../components/SkillList';
 import LanguageList from '../components/LanguageList';
 
-const info = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
-  'Amet autem beatae cum dolorum eum fugit maxime, natus necessitatibus ' +
-  'neque nostrum perferendis quia voluptatibus.';
+const Skills = (props) => {
+  const resume = props.resume || {};
+  const { skills_title, skills, langs, skills_info } = resume;
 
-const items = [
-  { id: 1, title: 'PHP', value: 90, color: 'yellow' },
-  { id: 2, title: 'HTML 5 & CSS 3', value: 70, color: 'orange' },
-  { id: 3, title: 'NodeJS', value: 90, color: 'purple' },
-  { id: 4, title: 'EcmaScript', value: 90, color: 'red' },
-  { id: 5, title: 'MySQL', value: 70, color: 'blue' },
-  { id: 6, title: 'Git', value: 100, color: 'green' },
-];
+  return (
+    <Section title={skills_title || ''}>
+      <SectionInfo title="Habilidades e Experiências" description={skills_info || ''} />
+      <SkillList items={skills || []} />
+      <Container style={{ paddingTop: 50 }}>
+        <SectionInfo title="Idiomas" description={skills_info || ''} />
+        <LanguageList items={langs || []} />
+      </Container>
+    </Section>
+  );
+};
 
-const langs = [
-  { id: 1, title: 'Português', value: 100, color: 'red' },
-  { id: 2, title: 'Inglês', value: 70, color: 'purple' },
-  { id: 3, title: 'Francês', value: 20 },
-  { id: 4, title: 'Japonês', value: 20, color: 'orange' },
-];
+const mapStateToProps = state => ({ resume: state.resumeReducer.resume });
 
-const Skills = () => (
-  <Section title="Minhas Habilidades">
-    <SectionInfo title="Habilidades e Experiências" description={info} />
-    <SkillList items={items} />
-    <Container style={{ paddingTop: 50 }}>
-      <SectionInfo title="Idiomas" description={info} />
-      <LanguageList items={langs} />
-    </Container>
-  </Section>
-);
-
-export default Skills;
+export default connect(mapStateToProps)(Skills);
