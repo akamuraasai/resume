7e6987971445528b0938a26f6029a7f12cb1fa8c
Jonathan Willian <jonathan.willian.tod@gmail.com>
2019-05-15 18:05:21 -0300

    Updated react version.

--- src/base/components/LanguageList.jsx
+++ src/base/components/LanguageList.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from 'semantic-ui-react/dist/es/collections/Grid/Grid';
 import Header from 'semantic-ui-react/dist/es/elements/Header/Header';
-import CircularProgressbar from 'react-circular-progressbar';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import '../assets/CircularProgress.css';
 
 const makeLanguage = items =>
   items.map(item => (
     <Grid.Column key={item.id}>
       <CircularProgressbar
-        percentage={item.value}
+        value={item.value}
+        text={`${item.value}%`}
         strokeWidth={2}
         initialAnimation
         className={item.color ? `progressbar-${item.color}` : ''}
